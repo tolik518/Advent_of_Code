160a90e5b5f230725e43e18398601a5e07a74fe5
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2022-12-01 10:44:27 +0100

    fixed script for generating the readme

--- script/get_readme.rb
+++ script/get_readme.rb
@@ -10,25 +10,27 @@ AOC = "https://adventofcode.com/"
 def download_readme(year, day)
 
   dirname = "%s/%02d/" % [year, day]
-  filename = 'README.HTML'
+  html_filename = 'README.HTML'
   url = AOC + year + "/day/" + day
 
   FileUtils.mkdir_p(dirname)
 
-  File.open(dirname + filename, "wb") do |readme_html_file|
+  File.open(dirname + html_filename, "wb") do |readme_html_file|
       response = HTTParty.get(url)
-      readme_html_file.write(response.body)
+      content = Nokogiri::HTML.parse(response.body)
+      readme_html_file.write(content.at("article").inner_html)
       if (response.code == 200) then
         puts "Downloaded..."
       else
+        puts "Looks like there is no puzzle for today (yet?)"
         puts "Download failed: %s - %s" % [response.code, response.message]
       end
   end
-  convert_to_markdown(dirname, filename)
+  convert_to_markdown(dirname, html_filename)
 end
 
-def convert_to_markdown(dirname, filename)
-  file_data = File.read(dirname + filename)
+def convert_to_markdown(dirname, html_filename)
+  file_data = File.read(dirname + html_filename)
 
   ReverseMarkdown.config do |config|
     config.unknown_tags     = :bypass
@@ -37,12 +39,24 @@ def convert_to_markdown(dirname, filename)
   end
 
   result = ReverseMarkdown.convert(file_data)
+  clean_markdown = result.inspect[1..-2].gsub("\\n", "\n")
 
   File.open(dirname + 'README.MD', "wb") do |readme_file|
-    readme_file.write(result.inspect)
+    readme_file.write(clean_markdown)
   end
+
+  File.delete(dirname + html_filename)
 end
 
+YEAR = Time.new.year
+MONTH = Time.new.month
+DAY = Time.new.day
 
-download_readme("2021", "1")
+if MONTH != 12
+  puts "Sorry, you'll have to wait for december"
+elif DAY > 25
+  puts "Looks like the AoC ended some days ago"
+else
+  download_readme(YEAR.to_s, DAY.to_s)
+end
 
