f8bc25bb781f6516e3057806c3fbc9961c0823f6
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2022-12-02 07:56:10 +0100

    day 02, part 2 solution

--- 2022/02/solution.rb
+++ 2022/02/solution.rb
@@ -1,39 +1,81 @@
 YEAR = 2022
 DAY  = 2
 
-html_filename = "%s/%02d/input.txt" % [YEAR, DAY]
+INPUT_FILENAME = "%s/%02d/input.txt" % [YEAR, DAY]
 
-sum = 0
-
-conditions = {
+WIN_CONDITIONS = {
   "A": "C",  # Rock vs Scissors
   "B": "A",  # Paper vs Rock
   "C": "B"   # Scissors vs Paper
 }
 
-File.open(html_filename).each(sep="\n") do |line|
-  line = line.sub('X', 'A') # Rock
-  line = line.sub('Y', 'B') # Paper
-  line = line.sub('Z', 'C') # Scissors
+LOSE_CONDITIONS = WIN_CONDITIONS.invert
 
-  opponent = line[0];
-  response = line[2];
+POINTS_FOR_SYMBOL = {
+  "A": 1,
+  "B": 2,
+  "C": 3,
+}
 
-  if response    == "A" # Rock
-    sum += 1
-  elsif response == "B" # Paper
-    sum += 2
-  elsif response == "C" # Scissors
-    sum += 3
+POINTS_FOR_CONDITION = {
+  "win":  6,
+  "draw": 3,
+  "lose": 0,
+}
+
+CONDITIONS = {
+  "X": "lose",
+  "Y": "draw",
+  "Z": "win"
+}
+
+def calculate_points_part1()
+  sum = 0
+  File.open(INPUT_FILENAME).each(sep="\n") do |line|
+    line = line.sub('X', 'A').sub('Y', 'B').sub('Z', 'C')
+
+    opponent = line[0];
+    response = line[2];
+
+    sum += POINTS_FOR_SYMBOL[response.to_sym]
+
+    if WIN_CONDITIONS[response.to_sym] == opponent
+      sum += POINTS_FOR_CONDITION[:win]
+    elsif opponent == response
+      sum += POINTS_FOR_CONDITION[:draw]
+    else
+      sum += POINTS_FOR_CONDITION[:lose]
+    end
   end
-
-  if conditions[response.to_sym] == opponent
-    sum += 6 #win
-  elsif opponent == response
-    sum += 3 #draw
-  else
-    sum += 0 #lost
-  end
+  puts sum.to_s
 end
 
-puts sum.to_s
+def calculate_points_part2()
+  sum = 0
+  File.open(INPUT_FILENAME).each(sep="\n") do |line|
+    opponent = line[0];
+    response = line[2];
+
+    if CONDITIONS[response.to_sym] == "lose"
+      response = response.sub(response, WIN_CONDITIONS[opponent.to_sym])
+    elsif CONDITIONS[response.to_sym] == "draw"
+      response = response.sub(response, opponent)
+    elsif CONDITIONS[response.to_sym] == "win"
+      response = response.sub(response, LOSE_CONDITIONS[opponent].to_s)
+    end
+
+    puts response
+    sum += POINTS_FOR_SYMBOL[response.to_sym]
+
+    if WIN_CONDITIONS[response.to_sym] == opponent
+      sum += POINTS_FOR_CONDITION[:win]
+    elsif opponent == response
+      sum += POINTS_FOR_CONDITION[:draw]
+    else
+      sum += POINTS_FOR_CONDITION[:lose]
+    end
+  end
+  puts sum.to_s
+end
+
+calculate_points_part2()
