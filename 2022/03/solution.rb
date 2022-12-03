78b5facab3105a020f5d2bc3388e2e4b47c8bfd8
Anatolij Vasilev <Anatolij.Vasilev@flyeralarm.com>
2022-12-03 10:21:48 +0100

    day 03, part 1&2 solution

---
+++ 2022/03/solution.rb
@@ -0,0 +1,37 @@
+YEAR = 2022
+DAY  = 3
+
+INPUT_FILENAME = "%s/%02d/input.txt" % [YEAR, DAY]
+
+def cut_in_half(string)
+  cut_in_half_regex = /.{#{string.length / 2}}/
+  string.partition(cut_in_half_regex).drop(1) # drop 1 because the first item is not needed
+end
+
+def calculate_for_half_lines()
+  sum = 0
+  File.open(INPUT_FILENAME).each(sep="\n") do |line|
+    first_half, second_half = cut_in_half(line)
+    duplicate_letter = (first_half.chars & second_half.chars).join
+    value = (duplicate_letter.ord - 96) % 58
+    sum += value
+  end
+  puts sum.to_s
+end
+
+def calculate_for_three_lines()
+  sum = 0
+  strings = []
+  File.open(INPUT_FILENAME).each(sep="\n") do |line|
+    strings.append(line)
+    if strings.count == 3
+      duplicate_letter = (strings[0].chars & strings[1].chars & strings[2].chars).join
+      value = (duplicate_letter.ord - 96) % 58
+      sum += value
+      strings = []
+    end
+  end
+  puts sum.to_s
+end
+
+calculate_for_three_lines()
